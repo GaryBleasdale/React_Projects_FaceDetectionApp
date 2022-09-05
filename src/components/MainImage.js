bd77b051adcc1dfdf5ea11fdb3a5f3d35a2a5c96
Gary Malcolm Bleasdale <gary@bleasdale.dev>
2022-09-05 13:50:03 -0300

    added error handling to image tag

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -1,16 +1,11 @@
 import React from "react";
 
 export default function MainImage({url}){
-   
-
 
   return (
     <div>
-    { url && url !== '' &&
-      <img src={`${url}`} alt='selected' onError={({ currentTarget }) => {
-        currentTarget.onerror = null;
-        currentTarget.src=`${url}`;
-      }} />
+    { url &&
+      <img src={`${url}`} alt='selected' onError={e => { e.currentTarget.src = "https://www.wpbeginner.com/wp-content/uploads/2017/08/defaultthumbnail.png"; }} />      
     }
     </div>
   )
